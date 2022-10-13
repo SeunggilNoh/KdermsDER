81131b59b58f7e2e0e13c3601b35ccf1acbc8a04
seunggil <seunggil.noh@gmail.com>
2022-10-13 13:11:00 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+## License
+__authour__ = "Jun-Hyeok Kim, Wooyoung Cho, SeungGil Noh"
+__copyright__ = "Copyright 2022, Gwangju Institute of Science and Technology, and Korea Electric Power Research Institute"
+__credits__ = ["Jun-Hyeok Kim", "Wooyoung Cho", "SeungGil Noh"]
+__version__ = "2.03"
+__maintainer__ = "Jun-Hyeok Kim"
+__email__ = "junhyeok8407@gm.gist.ac.kr"
+__status__ = "Update Functions and Refactoring"
+"""
